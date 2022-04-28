ebb3eef2ab9393edc8bef21dbd2027d484f6e5b1
kwynncom <buessresume@gmail.com>
2022-04-28 00:11:30 -0400

    yes, the list needs to be in the constructor.  Duh.

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -8,9 +8,15 @@ contract NFT is ERC721, PullPayment, Ownable, myNFTConfig {
     /* counters stuff */ using Counters for Counters.Counter; Counters.Counter private currentTokenId;
 
     function withdrawPayments(address payable payee) public override onlyOwner virtual { super.withdrawPayments(payee); }    
-    constructor() ERC721("NFTTutorial", "NFT") {     }
+    
+    whitelist whitelv;
+    freelist freelv;
+        
+    constructor() ERC721("NFTTutorial", "NFT") {
+     	whitelv = new whitelist();   	
+   	freelv  = new freelist();
+    }
     function freeMint() public returns (uint256)  {
-   	freelist freelv  = new freelist();
    	address addr = msg.sender;
     	freelv.		listCanMintOrDie(addr);
     	uint256 tokenN = preApprovedMint(addr);	 
@@ -18,7 +24,7 @@ contract NFT is ERC721, PullPayment, Ownable, myNFTConfig {
     	return tokenN;    	
     }
     function whitelistMint() public payable returns (uint256) {
-    	whitelist whitelv = new whitelist();
+
      	address addr = msg.sender;
   	whitelv.listCanMintOrDie(        addr);
     	uint256 tokenN = 	paidMint(addr, msg.value);	 
@@ -26,7 +32,7 @@ contract NFT is ERC721, PullPayment, Ownable, myNFTConfig {
     	return tokenN;
     }
     function generalMint()    	    public  payable returns (uint256) {     
-    	require(block.timestamp >= generalSalesStart, "general public post-whitelist sale has not yet begun");
+    	require(block.timestamp >= generalSalesStart, "general or post-whitelist sale has not yet begun");
     	return paidMint(msg.sender, msg.value);
     }
     function paidMint(address addr, uint256 msgValue) private returns (uint256) {
--- contracts/whitelist.sol
+++ contracts/whitelist.sol
@@ -5,6 +5,8 @@ import "./list.sol";
 
 contract whitelist is mylist {
 
+    constructor() { setList(); }
+
     function setList() private {
 	thelistv[0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097] = 2;	
     }

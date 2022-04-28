b29e06534bff9104cfde19b517c7852424a35136
kwynncom <buessresume@gmail.com>
2022-04-27 22:17:26 -0400

    whitelist inherits from general list

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/security/PullPayment.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./list.sol";
 import "./config.sol";
+import "./whitelist.sol";
 
 
 contract NFT is ERC721, PullPayment, Ownable, myNFTConfig {
@@ -19,35 +20,23 @@ contract NFT is ERC721, PullPayment, Ownable, myNFTConfig {
      mapping(address => uint8) private whitelistV;
 
     enum mintTypeEnum {free, whitelist, publicMint }
-    uint64 public constant whitelistStart  = 1651017400;
-    uint64 public constant publicStart  = 1651017400 + 86400;
     
-    mylist private wlv;
+    whitelist private whitelv;
+//    mylist private freelv;
     
 
     constructor() ERC721("NFTTutorial", "NFT") {
     
-   	wlv = new mylist();
-    
-        setFreelist();
-        setWhitelist();
+   	whitelv = new whitelist();
+//   	freelv  =
      }
-
-    function setFreelist() private {
-	freelistV[0xFABB0ac9d68B0B445fB7357272Ff202C5651694a] = 1;
-    }
-
-	function setWhitelist() private {
-		whitelistV[0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097] = 2;	
-	}
-
 
     function freeMint() 
     public 
     returns (uint256)
     {
-    	require(freelistV[msg.sender] >= 1, "The message sender is either not in the free list or has already used the free mint");
-    	return preApprovedMint(mintTypeEnum.free);
+//    	require(freelistV[msg.sender] >= 1, "The message sender is either not in the free list or has already used the free mint");
+//    	return preApprovedMint(mintTypeEnum.free);
     }
 
 
@@ -56,11 +45,10 @@ contract NFT is ERC721, PullPayment, Ownable, myNFTConfig {
     payable
     returns (uint256)
     {
-	wlv.listCanMintOrDie(msg.sender, msg.value);
-    
-    	require(block.timestamp >= whitelistStart, "whitelist sale has not started yet");
-// 	require(whitelistV[msg.sender] >= 1, "The message sender is either not in the whitelist or has already used whitelist quota");
-    	return preApprovedMint(mintTypeEnum.whitelist);	 
+	whitelv.listCanMintOrDie(msg.sender, msg.value);
+    	uint256 tokenN = paidMint();	 
+    	whitelv.decrement;
+    	return tokenN;
     }
 
     function paidMint()
@@ -69,10 +57,10 @@ contract NFT is ERC721, PullPayment, Ownable, myNFTConfig {
     returns (uint256)
     {
 	require(msg.value == MINT_PRICE, "Transaction value did not equal the mint price");
-	return preApprovedMint(mintTypeEnum.publicMint);    	
+	return preApprovedMint();    	
     }
 
-    function preApprovedMint(mintTypeEnum mintTyIn)
+    function preApprovedMint()
         private
         returns (uint256)
     {
@@ -81,12 +69,7 @@ contract NFT is ERC721, PullPayment, Ownable, myNFTConfig {
         currentTokenId.increment();
         uint256 newItemId = currentTokenId.current();
         
-        address sendTo = msg.sender;
-        
-        _safeMint(sendTo, newItemId);
-//        if      (mintTyIn == mintTypeEnum.free)      decFree (sendTo);
-        if (mintTyIn == mintTypeEnum.whitelist) wlv.decrement(sendTo);
-
+        _safeMint(msg.sender, newItemId);
         return newItemId;
     }
 }
--- contracts/list.sol
+++ contracts/list.sol
@@ -4,19 +4,18 @@ import "./config.sol";
 
 contract mylist is myNFTConfig {
 
-    mapping(address => uint8) private thelistv;
+    mapping(address => uint8) internal thelistv;
     
     constructor() {
-    	setList();
-    }
-
-    function setList() private {
-	thelistv[0xdF3e18d64BC6A983f673Ab319CCaE4f1a57C7097] = 2;	
+//    	setList();
     }
 
     function decrement(address a) public { thelistv[a]--; }
     
     function listCanMintOrDie(address a, uint256 msgValue) public view {
+    
+       	require(block.timestamp >= presalesStart && block.timestamp < publicSalesStart, "whitelist sale has not started yet");
+    
 	require(msgValue == MINT_PRICE, "Transaction value did not equal the mint price");
 	require(thelistv[a] >= 1, "The message sender address is either not in the list or has already used its quota");
      }

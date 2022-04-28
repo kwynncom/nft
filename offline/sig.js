26f68ecb6b815c1a73ba5579b1238931c7072187
kwynncom <buessresume@gmail.com>
2022-04-28 18:23:11 -0400

    all 3 buttons tested to some degree

--- offline/sig.js
+++ offline/sig.js
@@ -1,13 +1,53 @@
 // require('./../node_modules/web3');
 const abilo = require('web3-eth-abi');
 
-console.log(
+console.log('white ' + 
 abilo.encodeFunctionSignature(
 
 
     {
       "inputs": [],
       "name": "whitelistMint",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "payable",
+      "type": "function"
+    }
+
+)
+);
+
+console.log('free ' + 
+abilo.encodeFunctionSignature(
+
+    {
+      "inputs": [],
+      "name": "freeMint",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+
+)
+);
+
+console.log('general ' + 
+abilo.encodeFunctionSignature(
+
+{
+      "inputs": [],
+      "name": "generalMint",
       "outputs": [
         {
           "internalType": "uint256",

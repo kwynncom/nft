52ffa46dc4695c76975742b574bd70ae6ef902bb
kwynncom <buessresume@gmail.com>
2022-04-28 17:48:08 -0400

    starting to separate mint types in web

--- contracts/config.sol
+++ contracts/config.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 contract myNFTConfig {
     uint16 constant TOTAL_SUPPLY = 2222;
     uint256 public constant MINT_PRICE = 0.08 ether;
-    uint64 public constant presalesStart     = 1651119000;
+    uint64 public constant presalesStart     = 1651182329;
     uint64 public constant generalSalesStart = presalesStart + 600;
     
 }

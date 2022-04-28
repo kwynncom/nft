204f74ae6e7fcb0114b50d79543df4181cda153b
kwynncom <buessresume@gmail.com>
2022-04-27 22:43:01 -0400

    lists only need to be loaded if that function is called

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,60 +1,46 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.9;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/security/PullPayment.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "./config.sol";
-import "./whitelist.sol";
-import "./freelist.sol";
-
+import "./config.sol"; import "./whitelist.sol"; import "./freelist.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";  import "@openzeppelin/contracts/utils/Counters.sol"; 
+import "@openzeppelin/contracts/security/PullPayment.sol"; import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFT is ERC721, PullPayment, Ownable, myNFTConfig {
-    using Counters for Counters.Counter;
-    Counters.Counter private currentTokenId;
+    /* counters stuff */ using Counters for Counters.Counter; Counters.Counter private currentTokenId;
 
     function withdrawPayments(address payable payee) public override onlyOwner virtual { super.withdrawPayments(payee); }    
 
-    whitelist private whitelv;
-    freelist  private freelv;
-
-    constructor() ERC721("NFTTutorial", "NFT") {
-       	whitelv = new whitelist();
-   	freelv  = new freelist();
-     }
+    constructor() ERC721("NFTTutorial", "NFT") {     }
 
     function freeMint() public returns (uint256)  {
-
+   	freelist freelv  = new freelist();
    	address addr = msg.sender;
-    	freelv.listCanMintOrDie(addr);
-    	uint256 tokenN = preApprovedMint();	 
-    	freelv.decrement(addr);
+    	freelv.		listCanMintOrDie(addr);
+    	uint256 tokenN = preApprovedMint(addr);	 
+    	freelv.		       decrement(addr);
     	return tokenN;    	
     }
 
     function whitelistMint() public payable returns (uint256) {
     
-    	address addr = msg.sender;
-  	whitelv.listCanMintOrDie(addr, msg.value);
-    	uint256 tokenN = paidMint();	 
-    	whitelv.decrement(addr);
+    	whitelist whitelv = new whitelist();
+     	address addr = msg.sender;
+  	whitelv.listCanMintOrDie(	     addr);
+    	uint256 tokenN = publicSalesPaidMint(addr);	 
+    	whitelv.		   decrement(addr);
     	return tokenN;
     }
 
-    function paidMint() public payable returns (uint256) {
+    function publicSalesPaidMint(address addr) public payable returns (uint256) {
 	require(msg.value == MINT_PRICE, "Transaction value did not equal the mint price");
-	return preApprovedMint();    	
+	return preApprovedMint(addr);    	
     }
 
-    function preApprovedMint() private returns (uint256)    {
+    function preApprovedMint(address addr) private returns (uint256)    {
         uint256 tokenId = currentTokenId.current();
         require(tokenId < TOTAL_SUPPLY, "Max supply reached");
         currentTokenId.increment();
         uint256 newItemId = currentTokenId.current();
-        
-        _safeMint(msg.sender, newItemId);
+        _safeMint(addr, newItemId);
         return newItemId;
     }
 }
